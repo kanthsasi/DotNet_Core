79455220c574317637ee973928dc13de863277a0
kanthsasi <kanthsasi2003@gmail.com>
2024-11-12 16:38:10 +0530

    Entity Framework Core

---
+++ EFCore/Method2.cs
@@ -0,0 +1,73 @@
+﻿/*
+using EFCore.Data;
+using EFCore.Models.Domain;
+
+namespace EFCore
+{
+    public class Program
+    {
+        
+        static void CreateEmployee(Employee employee)
+        {
+            using (var context = new AppDbContext())
+            {
+                context.Employees.Add(employee);
+                context.SaveChanges();
+                Console.WriteLine("Employee Added Successfully.");
+            }
+        }
+        static void ReadAllEmployees()
+        {
+            using (var context = new AppDbContext())
+            {
+                var employees = context.Employees.ToList();
+                Console.WriteLine("All Employees:");
+                foreach (var employee in employees)
+                {
+                    Console.WriteLine("Id:"+employee.EmployeeId+"Name:"+employee.FirstName+"Salary:"+employee.Salary);
+                }
+            }
+        }
+
+        static void UpdateEmployeeSalary(int id, long newSalary)
+        {
+            using (var context = new AppDbContext())
+            {
+                var employee = context.Employees.Find(id);
+                if (employee != null)
+                {
+                    employee.Salary = newSalary;
+                    //Change Tracking:
+                    context.ChangeTracker.DetectChanges();
+                    Console.WriteLine(context.ChangeTracker.DebugView.LongView);
+                    context.SaveChanges();
+                    Console.WriteLine("Employee Salary updated successfully.");
+                }
+            }
+        }
+
+        static void DeleteEmployee(int id)
+        {
+            using (var context = new AppDbContext())
+            {
+                var employee = context.Employees.Find(id);
+                if (employee != null)
+                {
+                    context.Employees.Remove(employee);
+                    context.SaveChanges();
+                    Console.WriteLine("Employee Deleted Successfully.");
+                }
+            }
+        }
+        static void Main()
+        {
+            CreateEmployee(new Employee { FirstName = "Rajini", LastName = "B", Salary = 25000, ManagerId = 3 });
+            ReadAllEmployees();
+            UpdateEmployeeSalary(9,90000);
+            DeleteEmployee(12);
+            Console.ReadLine();
+        }
+
+    }
+}
+*/
---
+++ EFCore/Transaction.cs
@@ -0,0 +1,45 @@
+﻿/*
+using EFCore.Data;
+
+using (var context = new AppDbContext())
+{
+    //Begin Transaction
+    using (var transaction = context.Database.BeginTransaction())
+    {
+        //Perform database operations
+        var employeeId = 3;
+        //Retrive the employee details,project,and manager to update
+        var employeeToUpdate = context.Employees.FirstOrDefault(e => e.EmployeeId == employeeId);
+        var employeeDetailsToUpdate = context.EmployeeDetails.FirstOrDefault(ed => ed.Id == employeeToUpdate.EmployeeId);
+
+        if (employeeToUpdate != null && employeeDetailsToUpdate != null)
+        {
+            try
+            {
+                //Update the employee,s last name and address
+                employeeToUpdate.LastName = "H";
+                employeeDetailsToUpdate.Address = "Singapore";
+
+                //Update manager details
+                employeeToUpdate.ManagerId = 3;
+                context.ChangeTracker.DetectChanges();
+                Console.WriteLine(context.ChangeTracker.DebugView.LongView);
+                Console.ReadLine();
+                context.SaveChanges();
+                //Commit the transaction
+                transaction.Commit();
+            }
+            catch (Exception)
+            {
+                transaction.Rollback();
+                throw;
+            }
+           
+        }
+        else
+        {
+            throw new Exception("New Exception");
+        }
+    }
+}
+*/
